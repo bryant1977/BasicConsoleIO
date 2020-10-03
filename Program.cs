246051b96d87fc6aa73596ab44f201bfdcb4bf24
Ebrahim Aly <ebrahim.aly@gmail.com>
2020-10-03 18:50:03 -0400

    Commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace basicconsoleio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Basic Console I/O ****");
+            GetUserData();
+            Console.ReadLine();
+        }
+        private static void GetUserData()
+        {
+            // Get name and age.
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+            Console.Write("Please enter your age: ");
+            string userAge = Console.ReadLine();
+
+            // Change echo color, just for fun.
+            ConsoleColor prevColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            // Echo to the console.
+            Console.WriteLine("Hello {0}! You are {1} years old.", userName, userAge);
+
+            //restore previous color.
+            Console.ForegroundColor = prevColor;
+
+        }
+    }
+}
